98dffcf309e80d868144eca58913c608088d84ac
odys1528 <jolanta.zakrzewska@orange.pl>
2019-07-15 19:18:19 +0200

    core data read and save

--- TaskStalker/MainViewController.swift
+++ TaskStalker/MainViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 import Firebase
 import ChameleonFramework
+import CoreData
 
 class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var tempArray = [StalkItem]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.separatorStyle = .none
+        
+        loadItems()
 
         if let user = Auth.auth().currentUser?.email {
             print(user)
@@ -35,12 +39,15 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         let alert = UIAlertController(title: "Add new stalk", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) {(action) in
-            let newItem = StalkItem()
-            newItem.email = email.text!
-            newItem.name = name.text!
-            newItem.stalkPeriod = period.text!
-            self.tempArray.append(newItem)
-            self.tableView.reloadData()
+            if name.text != nil && email.text != nil && period.text != nil {
+                let newItem = StalkItem(context: self.context)
+                newItem.email = email.text!
+                newItem.name = name.text!
+                newItem.stalkPeriod = period.text!
+                self.tempArray.append(newItem)
+                self.tableView.reloadData()
+                self.saveItems()
+            }
         }
         
         alert.addTextField {(alertTextField) in
@@ -78,18 +85,36 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "StalkCell")
-        cell.textLabel?.text = "\(self.tempArray[indexPath.row].name) (\(self.tempArray[indexPath.row].email))"
+        cell.textLabel?.text = "\(self.tempArray[indexPath.row].name!) (\(self.tempArray[indexPath.row].email!))"
         cell.detailTextLabel?.text = self.tempArray[indexPath.row].stalkPeriod
         
         cell.textLabel?.textColor = FlatSand()
         cell.detailTextLabel?.textColor = FlatSand()
-        cell.textLabel?.backgroundColor = FlatMintDark()
-        cell.detailTextLabel?.backgroundColor = FlatMintDark()
+        cell.textLabel?.backgroundColor = FlatMint()
+        cell.detailTextLabel?.backgroundColor = FlatMint()
         cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 17.0)
         cell.detailTextLabel?.font = UIFont.boldSystemFont(ofSize: 12.0)
-        cell.backgroundColor = FlatMintDark().darken(byPercentage: CGFloat(indexPath.row) / CGFloat(tempArray.count))
+        cell.backgroundColor = FlatMint().darken(byPercentage: CGFloat(indexPath.row) / CGFloat(tempArray.count))
         
         return cell
+    }
+    
+    func saveItems() {
+        do {
+            try self.context.save()
+        } catch {
+            print("Error saving context, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        let request: NSFetchRequest<StalkItem> = StalkItem.fetchRequest()
+        do {
+            tempArray = try self.context.fetch(request)
+        } catch {
+            print("Error fetching data from context, \(error)")
+        }
     }
 
 }
